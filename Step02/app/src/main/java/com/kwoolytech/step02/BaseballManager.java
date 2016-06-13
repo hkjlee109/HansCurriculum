d0756666f65db02c2a00e2ee488c95bb2ad95697
Harry <kwooly@Harrys-MacBook-Pro.local>
2016-06-13 23:57:13 +0900

    Impl: Drag and drop.

--- Step02/app/src/main/java/com/kwoolytech/step02/BaseballManager.java
+++ Step02/app/src/main/java/com/kwoolytech/step02/BaseballManager.java
@@ -1,6 +1,11 @@
 package com.kwoolytech.step02;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.content.Context;
@@ -29,14 +34,24 @@ public class BaseballManager {
 
         FrameLayout.LayoutParams param = new FrameLayout.LayoutParams(BallWidth, BallHeight,
                                                                       Gravity.NO_GRAVITY);
-
         param.leftMargin = random.nextInt(mainLayout.getWidth() - BallWidth);
         param.topMargin = random.nextInt(mainLayout.getHeight() - BallHeight);
 
         image.setLayoutParams(param);
+        image.setOnLongClickListener(baseballOnLongClickListener);
 
         mainLayout.addView(image);
 
         return;
     }
+
+    View.OnLongClickListener baseballOnLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(data, shadowBuilder, v, 0);
+            return true;
+        }
+    };
 }
--- Step02/app/src/main/java/com/kwoolytech/step02/MainActivity.java
+++ Step02/app/src/main/java/com/kwoolytech/step02/MainActivity.java
@@ -2,6 +2,7 @@ package com.kwoolytech.step02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -19,11 +20,30 @@ public class MainActivity extends AppCompatActivity {
                                   (FrameLayout)findViewById(R.id.frameLayout));
 
         findViewById(R.id.buttonThrow).setOnClickListener(buttonOnClickListener);
+        findViewById(R.id.imageViewGlove).setOnDragListener(gloveOnDragListener);
     }
 
     Button.OnClickListener buttonOnClickListener = new Button.OnClickListener() {
+        @Override
         public void onClick(View v) {
             baseballManager.ThrowBaseball();
         }
+    };
+
+    View.OnDragListener gloveOnDragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DROP:
+                    View view = (View)event.getLocalState();
+                    FrameLayout layout = (FrameLayout)view.getParent();
+                    layout.removeView(view);
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+
     };
 }
