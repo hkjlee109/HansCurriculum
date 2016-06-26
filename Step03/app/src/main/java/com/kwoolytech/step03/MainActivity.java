08b21c592ebb140f8bbf3128f903b70d5e4223a9
kwoolytech <kwooly.tech@gmail.com>
2016-06-26 10:24:02 +0900

    #9 Hello, Step03

---
+++ Step03/app/src/main/java/com/kwoolytech/step03/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kwoolytech.step03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
