6830529bcf294abb224d1d8247d4183f6bb9fe33
kwoolytech <kwooly.tech@gmail.com>
2016-08-19 13:10:31 +0900

    #11 Impl: Make the map pickable and marker draggable.

--- Step04/app/src/main/java/com/kwoolytech/step04/CommonTool.java
+++ Step04/app/src/main/java/com/kwoolytech/step04/CommonTool.java
@@ -4,4 +4,6 @@ public class CommonTool {
     public final static String WEATHERQUERYURL = "http://api.openweathermap.org/data/2.5/weather?";
     public final static String APIKEY = "&APPID=7c9bbeb8ddd8520e0d7a72bf796ff493";
     public final static String WEATHERICONURL = "http://openweathermap.org/img/w/";
+
+    public final static int CODE_PROCESS_MAP_LOCATION_REQUEST = 1001;
 }
--- Step04/app/src/main/java/com/kwoolytech/step04/MapsActivity.java
+++ Step04/app/src/main/java/com/kwoolytech/step04/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.kwoolytech.step04;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
@@ -9,18 +10,12 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-//public class MapsActivity extends Activity {
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_maps);
-//    }
-//}
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +28,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        map = googleMap;
 
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng location = new LatLng(38, 127);
+        map.addMarker(new MarkerOptions().position(location).draggable(true));
+        map.moveCamera(CameraUpdateFactory.newLatLng(location));
+
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng location) {
+                map.clear();
+                map.addMarker(new MarkerOptions().position(location).draggable(true));
+                map.moveCamera(CameraUpdateFactory.newLatLng(location));
+            }
+        });
+
+        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("Lat", marker.getPosition().latitude);
+                intent.putExtra("Lng", marker.getPosition().longitude);
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                setResult(Activity.RESULT_OK, intent);
+
+                finish();
+                return true;
+            }
+        });
     }
-
 }
